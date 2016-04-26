a6962f95f44083ff1388e65275902d565b6d38c7
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2016-04-25 21:15:32 -0600

    Document lib/http-error.js.

--- app/lib/http-error.js
+++ app/lib/http-error.js
@@ -1,12 +1,18 @@
 'use strict';
+/**
+ * @module lib/http-error
+ **/
+
 const HttpStatus = require('http-status-codes');
 
-
-function httpError(code) {
+/**
+ * Return error based on HTTP status code.
+ *
+ * @param {Number} code - HTTP status code.
+ * @return {Object} - Error object with HTTP status code and message.
+ */
+module.exports = (code) => {
   const err = new Error(HttpStatus.getStatusText(code));
   err.status = code;
   return err;
 }
-
-
-module.exports = httpError;
