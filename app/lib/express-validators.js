a9fc6cd22a196de36dd871df1b042a6cc6f998a2
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2016-04-26 06:45:11 -0600

    Document lib/express-validators.js.

--- app/lib/express-validators.js
+++ app/lib/express-validators.js
@@ -1,7 +1,21 @@
 'use strict';
+/**
+ * @module lib/express-validators
+ * @description Custom express validator data structure.
+ **/
+
+/**
+ * @namespace customValidators
+ * @property {Object} customValidators - Custom made ExpressJS form validators.
+ * @property {Object} customValidators.username - Username validator.
+ */
 module.exports = {
   customValidators: {
-    // username validator
+    /**
+     * Validate username.
+     * @param {String} value - String to evaluate.
+     * @return {Bool} - False if value contains spaces.
+     */
     username(value) {
       return !/\s/.test(value);
     },
