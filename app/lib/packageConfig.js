79a415219a794da33b11996d0e78689b126891e8
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2016-04-26 06:40:44 -0600

    Document lib/config.js and lib/packageConfig.js.

--- app/lib/packageConfig.js
+++ app/lib/packageConfig.js
@@ -1,4 +1,9 @@
 'use strict';
+/**
+ * @module lib/packageConfig
+ * @description Parses package file and exports an Object representation.
+ **/
+
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('./package.json'));
 module.exports = config;
