5fd03b9dab5f7f29e610b17d9b0f66474a775152
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2016-04-26 11:26:31 -0600

    Document routes/index.js.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,20 @@
 'use strict';
+/**
+ * @module routes/index
+ * @description ExpressJS route for homepage.
+ **/
 const express = require('express');
 const httpError = require('http-error');
 const packageConfig = require('packageConfig');
 const router = new express.Router();
 
-/* GET home page. */
+/**
+ * GET home page.
+ *
+ * Can respond to HTML or JSON requests.
+ * @function
+ * @name /index
+ */
 router.get('/', (req, res, next) => {
   // data to be passed to template
   const data = {
